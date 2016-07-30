8b8d3ece77c3b887a63441f130a68a31deb1b60c
Alexander <ignatenko2207@gmail.com>
2016-07-30 13:29:28 +0300

    Start project

---
+++ UniversityTimetable/src/com/smarterama/Teacher.java
@@ -0,0 +1,5 @@
+package com.smarterama;
+
+public class Teacher {
+
+}
---
+++ UniversityTimetable/src/com/smarterama/Timetable.java
@@ -0,0 +1,10 @@
+package com.smarterama;
+
+public class Timetable {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
